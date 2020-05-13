602565590bf0d49818b4be641b8259bdbd11e346
Daedra <foglzerika@gmail.com>
2020-05-12 23:11:37 -0500

    ref | Refactoring footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,28 +6,7 @@ const Footer = () =>{
 
         <footer className="bg-white">
             <div className="container mx-auto  px-8">
-
-                <div className="w-full flex flex-col md:flex-row py-6 justify-center text-center">
-
-
-
-                    <div className="flex-1">
-                        <p className="uppercase text-gray-500 md:mb-6">Github</p>
-
-                    </div>
-                    <div className="flex-1">
-                        <p className="uppercase text-gray-500 md:mb-6">Linkedin</p>
-
-                    </div>
-                    <div className="flex-1">
-                        <p className="uppercase text-gray-500 md:mb-6">Gitlab</p>
-
-                    </div>
-                    <div className="flex-1">
-                        <p className="uppercase text-gray-500 md:mb-6">Stack Overflow</p>
-
-                    </div>
-                </div>
+                
                 <div className="w-full flex text-center flex-col md:flex-row py-6 justify-center text-center">
                     <p className="text-gray-500 md:mb-6 text-center">Made with love <span role="img" aria-label="heart">&#128153;</span> by Ironhacker  <span  role="img" aria-label="girl">&#128105;</span> Analin Flores <span  role="img" aria-label="fire">&#128293;</span></p>
                 </div>
