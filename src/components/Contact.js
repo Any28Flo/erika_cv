851984be728e109b97a4efa6a96765bce8de4c84
Daedra <foglzerika@gmail.com>
2020-06-05 23:25:44 -0500

    fix| Adding Contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Contact = () =>{
+    return(
+        <section className="pt-20 pb-48 bg-white " id="skills">
+            <div className="container mx-auto px-4">
+                <div className="flex flex-wrap justify-center text-center mb-24">
+                    <div className="w-full lg:w-6/12 px-4 ">
+                        <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">Get In Touch</h1>
+
+
+                        <div className="w-full mb-4">
+                            <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+    )
+}
+export default Contact;
